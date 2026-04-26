17903451344186cef1b2bd39f028404d00b8296b
coolmadhu <111852269+coolmadhu@users.noreply.github.com>
2026-04-26 22:10:19 +0530

    Delete environments/dev/s3.tf

--- environments/dev/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "github_demo_bucket" {
-  bucket = "coolmadhu-prashant-demo-bucket-567891"
-
-  tags = {
-    Name        = "GitHubActionBucket"
-    Environment = "Dev"
-  }
-}
